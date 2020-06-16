d00c383ce0038b1a384479c33b223936325b29d2
David Szabo <dszabo@cloudera.com>
2020-06-16 12:08:46 +0200

    CB-7484 Import and event constructor fixes

--- freeipa/src/main/java/com/sequenceiq/freeipa/flow/freeipa/diagnostics/StartCollectionResponse.java
+++ freeipa/src/main/java/com/sequenceiq/freeipa/flow/freeipa/diagnostics/StartCollectionResponse.java
@@ -1,8 +1,8 @@
 package com.sequenceiq.freeipa.flow.freeipa.diagnostics;
 
 public class StartCollectionResponse extends AbstractDiagnosticsCollectionEvent {
-    public StartCollectionResponse(String selector, Long stackId, String accountId, String environmentCrn) {
-        super(selector, stackId, accountId, environmentCrn, operation.getOperationId());
+    public StartCollectionResponse(String selector, Long stackId, String accountId, String environmentCrn, String operationId) {
+        super(selector, stackId, accountId, environmentCrn, operationId);
     }
 
     public StartCollectionResponse(DiagnosticsCollectionEvent event) {
