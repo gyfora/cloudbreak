db184e24974ce8ad86ab05ca740162dacbf2e107
Arpad Farsang <afarsang@HW12482.local>
2020-06-29 10:49:21 +0200

    test on jenkins

---
+++ integration-test/apidefinitions/check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function check() {
+  for f in /json/$1-swagger*.json ; do
+    echo check to ${f}
+    swagger-diff -i ${f} /json/$1
+    status=$?
+    [ $status -eq 0 ] && echo "$f compatibility was successfull" || cstatus=1
+  done;
+}
+
+cstatus=0
+DataList="cloudbreak,freeipa,environment,datalake,redbeams,autoscale"
+Field_Separator=$IFS
+
+echo Cloudbreak swagger compatibility check
+IFS=,
+for val in $DataList; do
+  check $val
+done
+IFS=$Field_Separator
+
+exit $cstatus
+
+
