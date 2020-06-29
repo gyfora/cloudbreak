55eaf2c77b4f6094c4fa0ddb841015a55795d3ae
Arpad Farsang <afarsang@HW12482.local>
2020-06-29 13:09:43 +0200

    again

--- integration-test/apidefinitions/check.sh
+++ integration-test/apidefinitions/check.sh
@@ -3,7 +3,7 @@
 function check() {
   for f in /json/$1-swagger*.json ; do
     echo check to ${f}
-    swagger-diff -i ${f} /json/$1
+    swagger-diff -i ${f} /json/${1}.json
     status=$?
     [ $status -eq 0 ] && echo "$f compatibility was successfull" || cstatus=1
   done;
